8568ef720f49d3718d32fda00f441d5e4c9ead3b
Tobias Genannt <tobias.genannt@gmail.com>
2022-07-22 10:59:13 +0200

    Initial plugin setup

---
+++ README.md
@@ -0,0 +1,3 @@
+## Netbox Initializers Plugin
+
+Load data from YAML files into Netbox
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-initializers',
+    version='0.1',
+    description='Load initial data into Netbox',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
