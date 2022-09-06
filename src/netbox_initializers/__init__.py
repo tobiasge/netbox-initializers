32d5773674c48ef1db5f2d6fdaed2ecf32d5ff39
Tobias Genannt <tobias.genannt@gmail.com>
2022-09-06 14:44:18 +0200

    Closes #1: Added __init__.py to mangement folder
    Without this file the find_packages methond from setuptools doesn't find the command subdirectory and it's python files.

--- src/netbox_initializers/__init__.py
+++ src/netbox_initializers/__init__.py
@@ -5,7 +5,7 @@ class NetBoxInitializersConfig(PluginConfig):
     name = 'netbox_initializers'
     verbose_name = 'NetBox Initializers'
     description = 'Load initial data into Netbox'
-    version = '3.2.1'
+    version = '3.2.2'
     base_url = 'initializers'
     min_version = '3.2.0'
     max_version = '3.2.99'
