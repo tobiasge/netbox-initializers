9acdf4e005132a70efaefd22022c501b8c5f14fa
Tobias Genannt <tobias.genannt@gmail.com>
2022-10-13 09:46:12 +0200

    Prepare version 3.3.2 (#32)

--- src/netbox_initializers/__init__.py
+++ src/netbox_initializers/__init__.py
@@ -5,7 +5,7 @@ class NetBoxInitializersConfig(PluginConfig):
     name = "netbox_initializers"
     verbose_name = "NetBox Initializers"
     description = "Load initial data into Netbox"
-    version = "3.3.1"
+    version = "3.3.2"
     base_url = "initializers"
     min_version = "3.3.0"
     max_version = "3.3.99"
