027f6391726c3b6eb9c92eb444e77ecf4ec33eee
Tobias Genannt <tobias.genannt@gmail.com>
2022-12-15 14:02:24 +0100

    Compatible with Netbox 3.4

--- src/netbox_initializers/initializers/providers.py
+++ src/netbox_initializers/initializers/providers.py
@@ -1,4 +1,5 @@
 from circuits.models import Provider
+from ipam.models import ASN
 
 from . import BaseInitializer, register_initializer
 
@@ -13,10 +14,22 @@ class ProviderInitializer(BaseInitializer):
         for params in providers:
             custom_field_data = self.pop_custom_fields(params)
 
+            asn_number = params.pop("asn")
+            asn = ASN.objects.filter(asn=asn_number).first()
+            if asn is None:
+                print(
+                    "⚠️ Unable to create Provider '{0}': The ASN '{1}' is unknown".format(
+                        params.get("name"), asn_number
+                    )
+                )
+                continue
+
             matching_params, defaults = self.split_params(params)
             provider, created = Provider.objects.get_or_create(**matching_params, defaults=defaults)
 
             if created:
+                provider.asns.add(asn)
+                provider.save()
                 print("📡 Created provider", provider.name)
 
             self.set_custom_fields_values(provider, custom_field_data)
