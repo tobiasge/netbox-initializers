57794486f7ea7f72bede8adbab2fcae382a4322e
Tobias Genannt <tobias.genannt@qbeyond.de>
2025-01-15 07:32:34 +0100

    Prepare for Netbox 4.2

--- src/netbox_initializers/initializers/users.py
+++ src/netbox_initializers/initializers/users.py
@@ -1,3 +1,4 @@
+from django.utils.crypto import get_random_string
 from users.models import Token, User
 
 from netbox_initializers.initializers.base import BaseInitializer, register_initializer
@@ -13,7 +14,7 @@ class UserInitializer(BaseInitializer):
 
         for username, user_details in users.items():
             api_token = user_details.pop("api_token", Token.generate_key())
-            password = user_details.pop("password", User.objects.make_random_password())
+            password = user_details.pop("password", get_random_string(length=25))
             user, created = User.objects.get_or_create(username=username, defaults=user_details)
             if created:
                 user.set_password(password)
