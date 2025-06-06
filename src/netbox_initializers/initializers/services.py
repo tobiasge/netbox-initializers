4b15cbbcb21b398575e25be02890adacaf3d6612
Stuart Weickgenant <sweickge@cisco.com>
2025-06-06 10:30:45 -0700

    Implement this change from v4.3 The device and virtual_machine foreign keys on the Service model have been replaced with a generic parent relationship to support the assignment of services to FHRP groups as well.

--- src/netbox_initializers/initializers/services.py
+++ src/netbox_initializers/initializers/services.py
@@ -1,13 +1,16 @@
 from dcim.models import Device
 from ipam.models import Service
+from ipam.models import FHRPGroup
 from virtualization.models import VirtualMachine
+from django.contrib.contenttypes.models import ContentType
 
 from netbox_initializers.initializers.base import BaseInitializer, register_initializer
 
-MATCH_PARAMS = ["name", "device", "virtual_machine"]
+MATCH_PARAMS = ["name", "parent_type", "parent_id"]
 OPTIONAL_ASSOCS = {
     "device": (Device, "name"),
     "virtual_machine": (VirtualMachine, "name"),
+    "fhrp_group": (FHRPGroup, "name"),
 }
 
 
@@ -21,11 +24,19 @@ class ServiceInitializer(BaseInitializer):
         for params in services:
             tags = params.pop("tags", None)
 
-            for assoc, details in OPTIONAL_ASSOCS.items():
+            # Handle the required parent relationship
+            parent = None
+            for assoc, model in OPTIONAL_ASSOCS.items():
                 if assoc in params:
-                    model, field = details
-                    query = {field: params.pop(assoc)}
-                    params[assoc] = model.objects.get(**query)
+                    query = {"name": params.pop(assoc)}
+                    parent = model.objects.get(**query)
+                    break
+
+            if parent:
+                params["parent_type"] = ContentType.objects.get_for_model(parent)
+                params["parent_id"] = parent.id
+            else:
+                raise ValueError("A parent (device, virtual_machine, or fhrp group) must be specified for the service.")
 
             matching_params, defaults = self.split_params(params, MATCH_PARAMS)
             service, created = Service.objects.get_or_create(**matching_params, defaults=defaults)
