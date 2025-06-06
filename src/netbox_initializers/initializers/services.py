e9b005bc14335b28a989d2630234fc6b4c952145
Stuart Weickgenant <sweickge@cisco.com>
2025-06-06 11:05:53 -0700

    Fix object check hopefully

--- src/netbox_initializers/initializers/services.py
+++ src/netbox_initializers/initializers/services.py
@@ -6,13 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from netbox_initializers.initializers.base import BaseInitializer, register_initializer
 
-MATCH_PARAMS = ["name", "parent_type", "parent_id"]
-OPTIONAL_ASSOCS = {
-    "device": Device,
-    "virtual_machine": VirtualMachine,
-    "fhrp_group": FHRPGroup,
-}
-
+MATCH_PARAMS = ["name", "parent_object_type", "parent_object_id"]
 
 class ServiceInitializer(BaseInitializer):
     data_file_name = "services.yml"
@@ -24,19 +18,20 @@ class ServiceInitializer(BaseInitializer):
         for params in services:
             tags = params.pop("tags", None)
 
-            # Handle the required parent relationship
-            parent = None
-            for assoc, model in OPTIONAL_ASSOCS.items():
-                if assoc in params:
-                    query = {"name": params.pop(assoc)}
-                    parent = model.objects.get(**query)
-                    break
-
-            if parent:
-                params["parent_type"] = ContentType.objects.get_for_model(parent)
-                params["parent_id"] = parent.id
+            # Check for each parent type (device, virtual_machine, fhrp_group)
+            if "device" in params:
+                parent = Device.objects.get(name=params.pop("device"))
+            elif "virtual_machine" in params:
+                parent = VirtualMachine.objects.get(name=params.pop("virtual_machine"))
+            elif "fhrp_group" in params:
+                parent = FHRPGroup.objects.get(name=params.pop("fhrp_group"))
             else:
-                raise ValueError("A parent (device, virtual_machine, or fhrp group) must be specified for the service.")
+                raise ValueError(
+                    "A parent (device, virtual_machine, or fhrp_group) must be specified for the service."
+                )
+
+            params["parent_object_type"] = ContentType.objects.get_for_model(parent)
+            params["parent_object_id"] = parent.id
 
             matching_params, defaults = self.split_params(params, MATCH_PARAMS)
             service, created = Service.objects.get_or_create(**matching_params, defaults=defaults)
