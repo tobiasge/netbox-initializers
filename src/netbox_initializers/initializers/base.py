c88665aaa66ef4b0140dfd305cc97c10ede1c970
Chris Portman <cportman@iron.ore>
2026-09-12 14:21:31 +1000

    make idempotent

--- src/netbox_initializers/initializers/base.py
+++ src/netbox_initializers/initializers/base.py
@@ -143,6 +143,9 @@ class BaseInitializer:
         save = False
 
         for mac in mac_addresses:
+            if entity.mac_addresses.filter(mac_address=mac).first():
+                continue
+
             mac_address = MACAddress.objects.create(mac_address=mac, description=f"{entity} MAC Address")
             entity.mac_addresses.add(mac_address)
             save = True
